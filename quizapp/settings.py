a1aca100cc895e050acf2913a63ac5d10ed30d46
alhadh7 <153531189+alhadh7@users.noreply.github.com>
2024-12-12 23:12:07 +0530

    Update settings.py

--- quizapp/settings.py
+++ quizapp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-=()$a&vb3fy4205^c!j-6o*+stcr%l7@bo_l0=)mlac__l2wuu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
